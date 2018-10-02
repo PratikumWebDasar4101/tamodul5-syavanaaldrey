2ed5c0aedd81e9fe86caabab518027e960b64ed7
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-02 06:34:34 -0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,11 @@
 <?php 
+  $username = $_POST["username"];
+ 	$password = $_POST["password"];
 
-
-
-
-
+ 	if($username == "admin" && $password == 1234){
+ 		$_SESSION["username"] = $username;
+ 		header("Location: registrasi.php");
+ 	}else{
+ 		header("Location: login.php");
+ 	}
 ?>
