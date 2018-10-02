773b150879de3a51e253bd8b179de83c3f55b3de
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-02 06:33:49 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,42 @@
 <?php
-
-
-
-
+<html>
+<head>
+	<title>LOGIN</title>
+</head>
+<body>
+	<form action="loginProses.php" method="POST">
+	<table align="center">
+		<tr>
+			<td colspan="3">
+				<center>LOG IN</center>
+			</td>
+		<tr>
+			<td>
+				USERNAME
+			</td>
+			<td>
+				:
+			</td>
+			<td>
+				<input type="text" name="username">
+			</td>
+		<tr>
+			<td>
+				PASSWORD
+			</td>
+			<td>
+				:
+			</td>
+			<td>
+				<input type="password" name="password">
+			</td>
+		<tr>
+			<td colspan="3">
+				<center><input type="submit" name="submit" value="KIRIM"></center>
+			</td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
 ?>
