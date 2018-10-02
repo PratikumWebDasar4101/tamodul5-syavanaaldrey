e401d4965d782f3c957fa81acfdd37faaf879df6
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-02 06:36:55 -0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,27 @@
 <?php
+$nim		= $_POST['nim'];
+		$nama		= $_POST['nama'];
+		$email		= $_POST['email'];
+		$line		= "<br>";
 
-
-
-
-
+		if(strlen($nama) > 10){
+			
+			echo "Max length 25 word";
+			echo $line;
+			echo "<a href ='registrasi.php'>INPUT ULANG</a>";
+			echo $line;
+		}
+		if(!is_numeric($nim) || strlen((string)$nim) > 10){
+			echo "NIM harus angka dan max lenth 10";
+			echo $line;
+			echo "<a href ='registrasi.php'>INPUT ULANG</a>";
+			echo $line;
+		}
+		if(!preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z.]{2,5}$/",$email)){
+			
+			echo "Format salah";
+			echo $line;
+			echo "<a href ='registrasi.php'>INPUT ULANG</a>";
+			echo $line;
+		}
 ?>
